e30eec33ee7907675efc9d706e78e214bcb09ea5
Brian Diesel <brian@designcondition.com>
2020-04-17 16:35:46 -0400

    inital commit

---
+++ src/app.ts
@@ -0,0 +1,60 @@
+import * as fromStore from './store';
+import { renderTodos } from  './utils';
+
+const input = document.querySelector('input') as HTMLInputElement;
+const button = document.querySelector('button') as HTMLButtonElement;
+const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
+const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const reducers = {
+  todos: fromStore.reducer
+};
+
+const store = new fromStore.Store(reducers);
+
+console.log(store.value)
+
+button.addEventListener(
+  'click',
+  () => {
+    if (!input.value.trim()) return;
+
+    const todo = { label: input.value, complete: false };
+
+    //console.log(payload);
+    // store.dispatch({
+    //   type: fromStore.ADD_TODO,
+    //   payload
+    // })
+    store.dispatch(new fromStore.AddTodo(todo));
+
+    console.log(store.value);
+
+    input.value = '';
+  },
+  false
+);
+
+
+//store.subscribe(state => console.log("STATE::::", state))
+const unsubscribe = store.subscribe(state => {
+  renderTodos(state.todos.data);
+});
+
+
+
+destroy.addEventListener('click', unsubscribe, false);
+
+
+todoList.addEventListener('click', function(event) {
+  const target = event.target as HTMLButtonElement;
+  if (target.nodeName.toLowerCase() === 'button') {
+    const todo = JSON.parse(target.getAttribute('data-todo') as any);
+    store.dispatch(new fromStore.RemoveTodo(todo));
+    //console.log(target);
+  }
+});
+
+store.subscribe(state => {
+  console.log("STATE::::::", state.todos.data);
+});
